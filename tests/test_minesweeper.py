5c6f2d279b3a7d4866e794d53c843a4038194e2a
Denis Wissocq <dw75fr@gmail.com>
2024-08-30 14:21:02 +0000

    Add failing test

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -9,3 +9,16 @@ def test_place_mines():
     game.place_mines()
     # TODO : Add assertions
     assert len(game.mines) == 2
+
+def test_reveal():
+    import random 
+    random.seed(0)
+    game = minesweeper.Minesweeper(3, 3, 2)
+    game.place_mines()
+    game.reveal(0, 0)
+    # TODO : Add assertions
+    assert game.board[0][0] == 1
+    assert game.revealed == {(0, 0)}
+
+def test_fail():
+    assert False
